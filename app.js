265e5a56ff99a09950acbf8d7a6a878d776fa658
Julio Perez <soyJulioLPJ@bitbucket.org>
2021-04-06 18:02:56 -0300

    recibir argumentos desde la linea de comandos

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { crearArchivo2 } = require('./helpers/multiplicar');
 
 console.clear();
-
-const base = 3;
+const [,,arg3 = 'base=5'] = process.argv;
+const [, base = 5] = arg3.split('=');
 
 crearArchivo2(base)
   .then( resp => console.log(resp))
