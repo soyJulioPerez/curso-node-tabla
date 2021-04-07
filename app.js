8a221333d24e3d6df9fc10558d5387b8814b986b
Julio Perez <soyJulioLPJ@bitbucket.org>
2021-04-07 09:46:16 -0300

    setup: add package.json

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const { crearArchivo2 } = require('./helpers/multiplicar');
+const argv = require('yargs')
+                .option('b', {
+                  alias: 'base',
+                  type: 'number',
+                  demandOption: true
+                })
+                .check( (argv, options) => {
+                  if (isNaN(argv.b)) throw 'La base tiene que ser un número';
+                  return true;
+                })
+                .argv;
 
 console.clear();
+console.log(argv);
+console.log('base: yargs', argv.base);
+
+const base = argv.base;
+/*
 const [,,arg3 = 'base=5'] = process.argv;
 const [, base = 5] = arg3.split('=');
+*/
 
 crearArchivo2(base)
   .then( resp => console.log(resp))
