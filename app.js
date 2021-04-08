d1a6c0442eee9a073e5fca2f54d088bad97a37b2
Julio Perez <soyJulioLPJ@bitbucket.org>
2021-04-08 07:57:25 -0300

    refactor: Mueve configuración de yargs a un archivo propio

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
 const { crearArchivo2 } = require('./helpers/multiplicar');
-const argv = require('yargs')
-                .option('b', {
-                  alias: 'base',
-                  type: 'number',
-                  demandOption: true
-                })
-                .option('l', {
-                  alias: 'listar',
-                  type: 'boolean',
-                  default: false
-                })
-                .check( (argv, options) => {
-                  if (isNaN(argv.b)) throw 'La base tiene que ser un número';
-                  return true;
-                })
-                .argv;
+const argv = require('./config/yargs');
 
 console.clear();
 
-const base = argv.base;
-const listar = argv.listar;
-/*
-const [,,arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
-*/
-
-crearArchivo2(base, listar)
+crearArchivo2(argv.base, argv.listar)
   .then( resp => console.log(resp))
   .catch( err => console.log(err))
 
-/* crearArchivo2()
-  .then( resp => console.log(resp))
-  .catch( err => console.log(err)) */
